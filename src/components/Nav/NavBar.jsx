94037c314848d6aab34f06783dd0838c4185c504
iKNZR <erickson.1207@gmail.com>
2024-03-22 12:28:27 -0500

    Added all zises and hamburger menu

--- src/components/Nav/NavBar.jsx
+++ src/components/Nav/NavBar.jsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import "./NavBar.css"
-import logo from '../../../public/assets/Logo.png'
+import React, { useState } from "react";
+import "./NavBar.css";
+import logo from "../../../public/assets/Logo.png";
 
 const NavBar = () => {
-  return (
-    <div className='navbar'>
-        <div className="logo"><img src={logo} alt="logo" /></div>
-        <div className="nav-links">
-            <a href="/">INICIO</a>
-            <a href="/senadores">SENADORES</a>
-            <a href="/diputados">DIPUTADOS</a>
-            <a href="/conecta">CONECTA YA!</a>
-        </div>
-    </div>
-  )
-}
+  const [menu, setMenu] = useState(false);
 
-export default NavBar
+  const handleActive = () => {
+    setMenu(!menu);
+  };
+  return (
+    <div className="navbar">
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <div className="nav-links">
+        <a href="/">INICIO</a>
+        <a href="/senadores">SENADORES</a>
+        <a href="/diputados">DIPUTADOS</a>
+        <a href="/conecta">CONECTA YA!</a>
+      </div>
+      <div className={menu ? "menu" : "hamburger"} onClick={handleActive}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+      {menu ? (
+        <div className="hamb-links">
+          <a href="/">INICIO</a>
+          <a href="/senadores">SENADORES</a>
+          <a href="/diputados">DIPUTADOS</a>
+          <a href="/conecta">CONECTA YA!</a>
+        </div>
+      ) : <></>}
+    </div>
+  );
+};
+
+export default NavBar;
