1b4d6a15ee911a12362ec48c33387c3d8a43a596
iKNZR <erickson.1207@gmail.com>
2024-05-09 20:53:32 -0500

    Timers erased

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./HomePage.css";
 import Hero from "../../components/Hero/Hero";
-import Banner from "../../components/Banner/Banner";
 import Diputados from "../../components/Diputados/Diputados";
 import Qr from "../../components/QR/Qr";
 import Footer from "../../components/Footer/Footer";
@@ -14,9 +13,7 @@ const HomePage = () => {
       <AboveHero />
       <Hero />
       <Button text="CONTÁCTANOS" />
-      {/* <Banner endDate="2024-03-31" displayText="PARA LA CAMPAÑA A GOBERNADOR" /> */}
       <Diputados />
-      <Banner endDate="2024-04-31" displayText="PROXIMAMENTE" />
       <Qr />
     </div>
   );
